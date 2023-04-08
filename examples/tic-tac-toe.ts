19072ad5f5e31f553e98e812932780d444fdccc3
AriPerkkio <ari.perkkio@gmail.com>
2023-04-08 19:46:19 +0300

    feat(tic-tac-toe): catch duplicate moves

--- examples/tic-tac-toe.ts
+++ examples/tic-tac-toe.ts
@@ -1,32 +1,43 @@
 /* Typings */
 type TicTacToe<
-  InitialBoard extends Board, // TODO: FirstMoveOrNextBoard
-  Moves extends Turn[] = []
+  MovesOrNextBoard extends Board | Turn[],
+  NextRoundsNextMoves extends Turn[] = [],
+  PreviousMark extends "X" | "O" | undefined = undefined,
+  CurrentBoard extends Board = ResolveBoard<MovesOrNextBoard>,
+  Moves extends Turn[] = ResolveNextMoves<MovesOrNextBoard, NextRoundsNextMoves>
 > =
   Moves extends [infer NextMove, ...infer NextMoves] ?
     NextMove extends Turn ?
-      Move<InitialBoard, NextMove> extends infer NextBoard ?
+      Move<CurrentBoard, NextMove, PreviousMark> extends infer NextBoard ?
         NextBoard extends Board ?
           CheckWin<NextBoard, "X"> extends true ? "X wins!"
             : CheckWin<NextBoard, "O"> extends true ? "O wins!"
               : CheckDraw<NextBoard> extends true ? "It's draw!"
               // It's valid move without win or draw
               : NextMoves extends Turn[] ?
-                TicTacToe<NextBoard, NextMoves>
-                : TicTacToe<NextBoard>
+                TicTacToe<NextBoard, NextMoves, NextMove["mark"]>
+                : TicTacToe<NextBoard, [], NextMove["mark"]>
         : NextBoard // It's invalid move, this will be error
       : "Unexpected error #001"
     : "Unexpected error #002"
-  : InitialBoard; // Just initial without move
+  : Moves extends [] ? CurrentBoard : "Unexpected error #003"
 
+type ResolveBoard<MovesOrNextBoard extends Board | Turn[]> =
+  MovesOrNextBoard extends Board ? MovesOrNextBoard : [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+type ResolveNextMoves<MovesOrNextBoard extends Board | Turn[], Moves extends Turn[]> =
+  MovesOrNextBoard extends Board ? Moves : MovesOrNextBoard
 
 type Move<
-  CurrentBoard extends Board = Board,
-  Move extends Turn = Turn,
+  CurrentBoard extends Board,
+  Move extends Turn,
+  PreviousMark extends "X" | "O" | undefined = undefined,
   CurrentCell extends Cell = CurrentBoard[Move["row"]][Move["cell"]]
-> = CurrentCell extends Empty
-  ? ReplaceRow<CurrentBoard, Move>
-  : AppendString<"Invalid move, cell used already: ", CurrentCell>;
+> = PreviousMark extends Move["mark"] ?
+  `Invalid move, ${PreviousMark} already run last round`
+    : CurrentCell extends Empty
+    ? ReplaceRow<CurrentBoard, Move>
+  : `Invalid move, Row ${Move["row"]} at cell ${Move["cell"]} already has ${CurrentCell}`
 
 type ReplaceRow<Rows extends Board, Move extends Turn> = Move["row"] extends 0
   ? [ReplaceCell<Rows[0], Move>, Rows[1], Rows[2]]
@@ -40,7 +51,6 @@ type ReplaceCell<Cells extends Row, Move extends Turn> = Move["cell"] extends 0
   ? [Cells[0], Move["mark"], Cells[2]]
   : [Cells[0], Cells[1], Move["mark"]];
 
-// prettier-ignore
 type CheckWin<CurrentBoard extends Board, Mark> =
   // Horizontal
   CurrentBoard[0][number] extends Mark ? true :
@@ -53,11 +63,10 @@ type CheckWin<CurrentBoard extends Board, Mark> =
   // Diagonal, TODO
   false;
 
-type CheckDraw<CurrentBoard extends Board> = CurrentBoard[Index][Index] extends
-  | "X"
-  | "O"
-  ? true
-  : false;
+type CheckDraw<CurrentBoard extends Board> =
+  CurrentBoard[Index][Index] extends "X" | "O"
+    ? true
+    : false;
 
 type Empty = " ";
 type Cell = "X" | "O" | Empty;
@@ -67,101 +76,85 @@ type Index = 0 | 1 | 2;
 type Turn = { mark: Exclude<Cell, Empty>; cell: Index; row: Index };
 
 /* Unrelated utility types */
-type AppendString<
+type LogInvalidMove<
   Prefix extends string,
   Postfix extends string
 > = `${Prefix}${Postfix}`;
 
-/* Implementation */
-function makeMove<B extends Board, T extends Turn>(_: B, __: T): Move<B, T> {
-  return "" as any;
-}
-
 /* Tests */
 function Equals<Expected, Actual extends Expected>(_: Expected, __: Actual) {}
+Equals<"Some text", "Some text">;
+// @ts-expect-error
+Equals<"Some text", "Some other text">;
 
-Equals(
-  makeMove(
+Equals<
+  Move<
     [
       [" ", " ", " "],
       [" ", " ", " "],
       [" ", " ", " "],
     ],
-    { mark: "X", cell: 0, row: 0 }
-  ),
+    { mark: "X", cell: 0, row: 0 },
+  >,
   [
     ["X", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "],
   ]
-);
-
-Equals(
-  makeMove(
-    [
-      ["X", " ", " "],
-      [" ", " ", " "],
-      [" ", " ", " "],
-    ],
-    { mark: "O", cell: 0, row: 0 }
-  ),
-  "Invalid move, cell used already: X"
-);
+>;
 
 Equals<
-  TicTacToe<
+  Move<
     [
       [" ", " ", " "],
+      [" ", " ", "O"],
       [" ", " ", " "],
-      [" ", " ", " "]
     ],
-    [
-      { mark: "X"; row: 0; cell: 0 },
-      { mark: "O"; row: 1; cell: 0 },
-      { mark: "X"; row: 0; cell: 1 },
-      { mark: "O"; row: 1; cell: 1 },
-      { mark: "X"; row: 0; cell: 2 }
-    ]
+    { mark: "O", row: 1, cell: 2 }
   >,
+  "Invalid move, Row 1 at cell 2 already has O"
+>;
+
+Equals<
+  TicTacToe<[
+    { mark: "X"; row: 0; cell: 0 },
+    { mark: "O"; row: 1; cell: 0 },
+    { mark: "X"; row: 0; cell: 1 },
+    { mark: "O"; row: 1; cell: 1 },
+    { mark: "X"; row: 0; cell: 2 },
+  ]>,
   "X wins!"
 >;
 Equals<CheckDraw<[["X", "O", "X"], ["O", "O", "X"], ["X", "X", "O"]]>, true>;
 Equals<CheckDraw<[[" ", "X", "X"], ["O", "O", "X"], ["O", "X", "O"]]>, false>;
 
 Equals<
-  TicTacToe<
-    [
-      [" ", " ", " "],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ],
-    [
-      { mark: "X"; row: 0; cell: 0 },
-      { mark: "O"; row: 1; cell: 0 },
-      { mark: "X"; row: 0; cell: 1 },
-      { mark: "O"; row: 1; cell: 1 },
-      { mark: "X"; row: 0; cell: 2 }
-    ]
-  >,
+  TicTacToe<[
+    { mark: "X"; row: 0; cell: 0 },
+    { mark: "O"; row: 1; cell: 0 },
+    { mark: "X"; row: 0; cell: 1 },
+    { mark: "O"; row: 1; cell: 1 },
+    { mark: "X"; row: 0; cell: 2 }
+  ]>,
   "X wins!"
 >;
 
 Equals<
-  TicTacToe<
-    [
-      [" ", " ", " "],
-      [" ", " ", " "],
-      [" ", " ", " "]
-    ],
-    [
-      { mark: "X"; row: 0; cell: 0 },
-      { mark: "O"; row: 1; cell: 0 },
-      { mark: "X"; row: 0; cell: 1 },
-    ]
-  >,
+  TicTacToe<[
+    { mark: "X"; row: 0; cell: 0 },
+  ]>,
   [
-    ["X", "X", " "],
-    ["O", " ", " "],
+    ["X", " ", " "],
+    [" ", " ", " "],
     [" ", " ", " "]
   ]
+>;
+
+
+Equals<
+  TicTacToe<[
+    { mark: "X"; row: 0; cell: 0 },
+    { mark: "X"; row: 0; cell: 1 },
+  ]>,
+  "Invalid move, X already run last round"
 >;
