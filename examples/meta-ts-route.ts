657097d5bcae5e4c5ac6f87bf661510f52d4ff23
AriPerkkio <ari.perkkio@gmail.com>
2023-04-16 20:24:55 +0300

    feat(route): query params

--- examples/meta-ts-route.ts
+++ examples/meta-ts-route.ts
@@ -1,25 +1,60 @@
 /* Typings */
-type PathParameters<Path extends string> = CaptureParameter<Path>;
+type PathParameters<Path extends string> = CapturePathParameters<Path>;
+type QueryParameters<Path extends string> = CaptureQueryParameters<Path>;
 
-type CaptureParameter<
+type CapturePathParameters<
   Path extends string,
-  PreviousParameters extends Record<never, string> = Record<never, string>
-> = Path extends `${infer _PathStart}:${infer ParameterEnd}`
-  ? ParameterEnd extends `${infer Parameter}/${infer PathEnd}`
-    ? CaptureParameter<PathEnd, AddToRecord<PreviousParameters, Parameter>>
-    : AddToRecord<PreviousParameters, ParameterEnd>
+  PreviousParameters extends Record<string, string> | undefined = undefined
+> = SplitFromEnd<
+  Path,
+  "?"
+> extends `${infer _PathStart}:${infer ParameterStart}`
+  ? ParameterStart extends `${infer Parameter}/${infer PathEnd}`
+    ? CapturePathParameters<PathEnd, AddToRecord<PreviousParameters, Parameter>>
+    : AddToRecord<PreviousParameters, ParameterStart>
+  : PreviousParameters;
+
+type CaptureQueryParameters<
+  Path extends string,
+  PreviousParameters extends Record<string, string> | undefined = undefined
+> = Path extends `${infer _PathStart}?${infer Query}`
+  ? CaptureQueryParameters<Query, PreviousParameters>
+  : Path extends `${infer QueryParam}=${infer RestQuery}`
+  ? CaptureQueryParameters<
+      RestQuery,
+      AddToRecord<PreviousParameters, SplitFromStart<QueryParam, "&">>
+    >
   : PreviousParameters;
 
 /* Utilities */
 type AddToRecord<
-  Current extends Record<string, string>,
+  Current extends Record<string, string> | undefined,
   Entry extends string
-> = Current & Record<Entry, string>;
+> = Current extends Record<string, string>
+  ? Record<Entry | keyof Current, string>
+  : Record<Entry, string>;
+
+type SplitFromStart<
+  Text extends string,
+  Character extends string
+> = Text extends `${infer Start}${Character}${infer Trimmed}`
+  ? SplitFromStart<Trimmed, Character>
+  : Text;
+
+type SplitFromEnd<
+  Text extends string,
+  Character extends string
+> = Text extends `${infer Trimmed}${Character}${infer End}`
+  ? SplitFromEnd<Trimmed, Character>
+  : Text;
 
 /* Implementation */
 function get<Path extends string>(
   path: Path,
-  callback: (request: { parameters: PathParameters<Path> }) => void
+  callback: (request: {
+    parameters: PathParameters<Path>;
+    query: QueryParameters<Path>;
+  }) => void
 ) {}
 
 /* Usage */
@@ -43,3 +78,15 @@ get("/api/users/:userId/accounts/:accountId", (request) => {
   // @ts-expect-error -- This does not exist
   request.parameters.clientId;
 });
+
+get(
+  "/api/users/:userId/accounts/:accountId?name=x&limit=y&count=z",
+  (request) => {
+    request.parameters.userId.toUpperCase();
+    request.parameters.accountId.toUpperCase();
+
+    request.query.limit.toUpperCase();
+    request.query.name.toUpperCase();
+    request.query.count.toUpperCase();
+  }
+);
---
+++ links.md
@@ -0,0 +1,5 @@
+Type system:
+
+- https://www.richard-towers.com/2023/03/11/typescripting-the-technical-interview.html
+- https://gist.github.com/acutmore/9d2ce837f019608f26ff54e0b1c23d6e
+- https://ricklove.me/typescript-type-system-adventure
